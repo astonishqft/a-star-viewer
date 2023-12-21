0942a9d6cebeb35253ba2251765f0b3535dd0a38
qi.futao <qi.futao@iwhalecloud.com>
2023-12-21 22:44:21 +0800

    init

---
+++ src/App.vue
@@ -0,0 +1,15 @@
+<script setup lang="ts">
+import { RouterView } from 'vue-router';
+</script>
+
+<template>
+  <RouterView />
+</template>
+
+<style scoped lang="less">
+body {
+  width: 100%;
+  height: 100%;
+}
+
+</style>
